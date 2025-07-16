13c557bb88cea9275da2a9145afe6bef5b4928da
KaioFelps <kaiofelipe2005@gmail.com>
2025-07-16 10:18:15 -0300

    feat: setup cookies and ejs template engine

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from "@nestjs/core";
 import { configDotenv } from "dotenv";
 import { AppModule } from "./app.module";
 import { EnvVarsService } from "./config/env/env.service";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "node:path";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   configDotenv();
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(cookieParser());
+
+  app.setViewEngine("ejs");
+  const viewsDir = join(__dirname, "..", "views");
+  app.setBaseViewsDir(viewsDir);
+
   const env = app.get(EnvVarsService);
 
   await app.listen(env.vars.app.port);
