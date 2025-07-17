d7572ad07d5e8d94eeb60caac3bc82569a810e6d
KaioFelps <kaiofelipe2005@gmail.com>
2025-07-17 11:34:20 -0300

    chore: move home controller to home module

--- src/infra/gateways/http/http.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { HomeController } from "./home.controller";
-import { LtiController } from "./lti.controller";
-import { OIDCController } from "./oidc.controller";
-
-@Module({
-  controllers: [OIDCController, LtiController, HomeController],
-})
-export class HttpModule {}
